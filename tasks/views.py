0ec0bbce16c9d99602a080db798ba77fabc00d98
notacoham <alexcottam12@gmail.com>
2025-07-15 12:00:51 -0600

    feat: add tasks view and add tasks to the urls

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Task
 
 # Create your views here.
+
+
+@login_required
+def task_list(request):
+  tasks = Task.objects.filter(user=request.user)
+
+  overdue_tasks = Task.objects.overdue(request.user)
+
+  context = {
+    'tasks': tasks,
+    'overdue_tasks': overdue_tasks
+  }
+
+  return render(request, 'tasks/task_list.html', context)
