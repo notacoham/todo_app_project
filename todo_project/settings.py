92cdc9ab15325a0ed76df4b2d20ec2617e01af85
notacoham <alexcottam12@gmail.com>
2025-07-15 12:40:48 -0600

    feat: implement task listing after login and fix logout

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -55,7 +55,10 @@ ROOT_URLCONF = 'todo_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'tasks', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
